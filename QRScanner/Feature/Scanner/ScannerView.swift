b9133dace2c14b36301631dc7920477d7331ef6a
Samet Karakurt <sametkarakurt@Samet-Air.home>
2022-09-29 15:18:52 +0300

    Folders Organized

--- QRScanner/Feature/Scanner/ScannerView.swift
+++ QRScanner/Feature/Scanner/ScannerView.swift
@@ -11,6 +11,7 @@ import CodeScanner
 struct ScannerView: View {
     @State private var isShowDetailScreen = false
     @State private var details = [String]()
+    @State private var scannedQRDetail = ScannedQRDetail(qrContent: "", type: "", data: "")
     var body: some View {
         NavigationView {
             VStack{
@@ -19,7 +20,7 @@ struct ScannerView: View {
                     EmptyView()
                 }
             }
-        
+            
         }
     }
     
@@ -27,6 +28,30 @@ struct ScannerView: View {
         switch result {
         case .success(let result):
             details =  result.string.components(separatedBy: "\n")
+            var type: String {
+                if details.contains("youtube") {
+                    return "Youtube"
+                } else if details.contains("twitter") {
+                    return "Twitter"
+                }
+                else if details.contains("instagram") {
+                    return "Instagram"
+                }
+                else if details.contains("whatsapp") {
+                    return "WhatsApp"
+                }
+                else if (details.contains("@") && details.contains(".com") ) {
+                    return "Mail"
+                }
+                else if details.contains("https") {
+                    return "webSite"
+                }
+                else {
+                    return "Text"
+                }
+            }
+            scannedQRDetail.type = type
+            scannedQRDetail.data = result
             isShowDetailScreen = true
         case .failure(let error):
             print("Scanning failed \(error.localizedDescription)")
---
+++ QRScanner/Resources/QR/ScannedQRDetail.swift
@@ -0,0 +1,15 @@
+//
+//  ScannedQRDetail.swift
+//  QRScanner
+//
+//  Created by Samet Karakurt on 29.09.2022.
+//
+
+import Foundation
+import UIKit
+
+struct ScannedQRDetail{
+    var qrContent: String[]
+    var type: String
+    var data: String
+}
